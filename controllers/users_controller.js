16c260ae06f7ff22f908518ac4eaad2af4dea1ab
anjanpreet <anjanpreetsingh1019@gmail.com>
2022-07-20 13:23:21 +0530

    Adding Notes.txt and users

---
+++ controllers/users_controller.js
@@ -0,0 +1,3 @@
+module.exports.profile=function(req,res){
+    return res.end("<h1>Sabh badiya challa hain!!");
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
-
+console.log("router1 loaded");
 module.exports=router;
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
